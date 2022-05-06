a1e878e390eb8f0119c8c2adf4ba684ba4b13e69
seiyarick <seiyahirota2@gmail.com>
2022-05-06 10:40:50 +0000

    コメントバリデーション

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    dish = Dish.find(params[:dish_id])
-    comment = Comment.new(comment_params)
-    comment.user_id = current_user.id
-    comment.dish_id = dish.id
-    if comment.save
-      redirect_to dish_path(dish.id), flash: {comment: "コメントしました"}
+    @dish = Dish.find(params[:dish_id])
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.dish_id = @dish.id
+    if @comment.save
+      @comment.create_notification_comment!(current_user, @comment.id)
+      redirect_to dish_path(@dish.id), flash: {comment: "コメントしました"}
     else
-      redirect_to dish_path(dish.id)
+      redirect_to dish_path(@dish.id)
     end
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :dish
+  has_many :notifications, dependent: :destroy
+
+  validates :comment_text, presence: true, length: {minimum: 1, maximum: 50}
+
   
- 
+
 end
