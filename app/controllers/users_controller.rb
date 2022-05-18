267e32875d0d7614752725316da31fa7299e60a3
seiyarick <seiyahirota2@gmail.com>
2022-05-18 02:52:03 +0000

    フォロ、ーフォロワー一覧ページの名前表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,13 +44,14 @@ class UsersController < ApplicationController
   end
 
   def followings
-    user = User.find(params[:id])
-    @users = user.followings
+    @user = User.find(params[:id])
+    @users = @user.followings
+
   end
 
   def followers
-    user = User.find(params[:id])
-    @users = user.followers
+    @user = User.find(params[:id])
+    @users = @user.followers
   end
 
   private
